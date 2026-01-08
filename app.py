8b0e6156dea82e262c2eeb1ab46d2e1fbcd384e6
ayaanwarsi-cmd <ayaanwarsi816@gmail.com>
2026-01-08 14:28:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -321,6 +321,11 @@ setInterval(()=>{geoText.innerHTML=geoLogs[gi];gi=(gi+1)%geoLogs.length},3500);
 @app.route("/")
 def index():
  return Response(HTML, mimetype="text/html")
+ 
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__=="__main__":
  app.run(host="0.0.0.0",port=5000)
+
